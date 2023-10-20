7e9cac575646b58f19036d6248d45324994af13d
ExName123 <exnew88@mail.ru>
2023-10-20 11:02:56 +0300

    first

---
+++ home.php
@@ -0,0 +1,36 @@
+<?php
+include('header.html');
+
+?>
+
+<div class="container">
+    <form action="index.php" method="GET">
+        <div class="form-group">
+            <?php
+            if (isset($_POST['name'])) {
+
+                $name = $_POST['name'];
+                $email = $_POST['email'];
+
+                echo '<p name="name">' . 'Здравствуйте, ' . $_POST['name'] . '</p>';
+                if ($_POST['category'] == 'proposal') {
+                    echo '<p>Спасибо за ваше предложение:</p>';
+                    echo '<textarea>' . $_POST['message'] . '</textarea>';
+                } else {
+                    echo '<p>Мы рассмотрим Вашу жалобу:</p>';
+                    echo '<textarea>' . $_POST['message'] . '</textarea>';
+                }
+                if (isset($_FILES['attachment'])) {
+                    $file = $_FILES['attachment'];
+                    $uploadedFile = $file['name'];
+                    echo '<p>Прикрепленный файл: </p>'. $uploadedFile;
+                }
+                else  echo '<p>Файл не прикреплен : </p>';
+            }
+            ?>
+        </div>
+        <!-- <a class="btn" href="index.php?N='.$_POST['name'].'&E='.$_POST['email'].'&S='.$_POST['agreement'].'">Заполнить снова</a> -->
+        <a class="btn" href="index.php?name=<?php echo $_POST['name']; ?>&email=<?php echo $_POST['email']; ?>&agreement=<?php echo $_POST['agreement']; ?>">Заполнить снова</a>
+        <!-- <input type="submit" value="Заполнить снова"> -->
+    </form>
+</div>
---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+include('header.html');
+    if(isset($_GET['name'])){
+        $name=$_GET['name'];
+        $email=$_GET['email'];
+        $agreement=$_GET['agreement'];
+    }
+?>
+<html>
+
+<body>
+    <h2 id="title-form">Форма обратной связи</h2>
+    <div class="container">
+        <form action="home.php" method="post" enctype="multipart/form-data">
+            <div class="form-group">
+
+                <label class="form-additional" for="name">ФИО</label>
+                <input class="input-data" name="name" id="name" type="text" value="<?php echo $name?>" required placeholder="ФИО">
+            </div>
+
+            <div class="form-group">
+                <label class="form-additional" for="email">Email</label>
+                <input class="input-data" id="email" type="text" value="<?php echo $email?>" name="email" placeholder="email">
+            </div>
+
+            <div class="form-group">
+                <label class="form-label" for="category">Категория обращения</label>
+                <select name="category" id="category">
+                    <option value="proposal">Предложение</option>
+                    <option value="grievance">Жалоба</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <p>Как вы узнали о нас?</p>
+
+                <div class="container-selector">
+                    <input type="radio" name="selector" id="advertising" value="advertising">
+                    <label class="label-selector" for="friends">Реклама в интернете</label>
+
+                </div>
+                <div class="container-selector">
+                    <input type="radio" name="selector" id="friends" value="friends">
+                    <label class="label-selector" for="friends">Рассказали знакомые</label>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="form-label" for="message">Текст сообщения</label>
+                <textarea name="message" id="message" cols="30" rows="10" placeholder="Введите текст"></textarea>
+            </div>
+            <div class="form-group">
+                <input checked="<?php if($agreement = 'yes') true ?>" type="checkbox" name="agreement" id="agreement" value="yes" required>
+                <label for="agreement">Даю согласие на обработку данных</label>
+            </div>
+            <div class="form-group">
+                <label class="form-label" for="attachment">Файл для отправки</label>
+                <input type="file" name="attachment" id="attachment">
+            </div>
+            <input type="submit" value="Отправить">
+        </form>
+    </div>
+</body>
+
+</html>
